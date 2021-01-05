03e2d97f562a1435f9259b4303cd49e2fe396872
Tyler Poole <tylerpoole@Tylers-Mac-mini.local>
2021-01-04 19:48:14 -0500

    Updates

---
+++ precache-manifest.c357175c8cf61dbad3e078cffec6ef21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cafaf5260b0da00b92ce414262b5aadf",
+    "url": "/mars/index.html"
+  },
+  {
+    "revision": "1558a6f0b0746a960192",
+    "url": "/mars/static/css/main.75729402.chunk.css"
+  },
+  {
+    "revision": "e3f4ee54e29a9df51086",
+    "url": "/mars/static/js/2.096a1411.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/mars/static/js/2.096a1411.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1558a6f0b0746a960192",
+    "url": "/mars/static/js/main.8364206b.chunk.js"
+  },
+  {
+    "revision": "9f2ab4dc83aa438c7f37",
+    "url": "/mars/static/js/runtime-main.bb618989.js"
+  },
+  {
+    "revision": "ee15e8fa5966ccb39347d58852d84d16",
+    "url": "/mars/static/media/nasa.ee15e8fa.svg"
+  }
+]);
